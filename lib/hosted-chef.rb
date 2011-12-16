6fa6ed91b82ed7c11e3c7d9560073ca4e868b472
Daniel DeLeo <dan@opscode.com>
2011-12-15 16:18:24 -0800

    fix csrf protection issue, config install now works

--- lib/hosted-chef.rb
+++ lib/hosted-chef.rb
@@ -2,6 +2,7 @@ require 'pp'
 require 'forwardable'
 require 'optparse'
 require 'rexml/document'
+require 'fileutils'
 
 require 'rubygems'
 require 'restclient'
@@ -104,7 +105,6 @@ module HostedChef
 
     def parse
       parser.parse!
-      options.ask_for_missing_opts
       options
     end
 
@@ -154,7 +154,16 @@ module HostedChef
     def user_key_authenticity_token
       @user_key_authenticity_token ||= begin
         new_key_page = RestClient.get("https://community.opscode.com/users/#{username}/user_key/new", :cookies => login_cookies)
+        @login_cookies = new_key_page.cookies
         extract_csrf_token_from(new_key_page)
+      end
+    end
+
+    def validator_key_authenticity_token
+      @validator_key_authenticity_token ||= begin
+        org_list_page = RestClient.get("https://manage.opscode.com/organizations", :cookies => login_cookies)
+        @login_cookies = org_list_page.cookies
+        extract_csrf_token_from(org_list_page)
       end
     end
 
@@ -187,31 +196,114 @@ module HostedChef
       RestClient.post("https://community.opscode.com/users/#{username}/user_key", post_options, {:cookies => login_cookies})
     end
 
+    def validator_key
+      #https://manage.opscode.com/organizations/prodbench2/_regenerate_key
+      post_options = {
+        #:multipart => true,
+        :authenticity_token => validator_key_authenticity_token,
+        :_method => "put"
+      }
+      headers = {
+        :Referer => "https://manage.opscode.com/organizations",
+        :cookies => login_cookies
+      }
+      uri = "https://manage.opscode.com/organizations/#{orgname}/_regenerate_key"
+      RestClient.post(uri, post_options,headers)
+    end
     private
 
     def extract_csrf_token_from(page)
       # TODO: serve the authenticity token over JSON so we don't have to
       # do this.
-      md = page.match(/(<input[^\>]*name=\"authenticity_token\"[^\>]+\>)/)
-      page_element = md[1]
-      xml = REXML::Document.new(page_element)
-      xml.elements.first.attributes["value"]
+      if md = page.match(/(\<meta content=\"[^\"]+\" name=\"csrf-token\"[\s]*\/\>)/)
+        page_element = md[1]
+        xml = REXML::Document.new(page_element)
+        xml.elements.first.attributes["content"]
+      elsif md = page.match(/(<input[^\>]*name=\"authenticity_token\"[^\>]+\>)/)
+        page_element = md[1]
+        xml = REXML::Document.new(page_element)
+        xml.elements.first.attributes["value"]
+      else
+        raise "Can't find CSRF token on the page:\n#{page}"
+      end
     end
   end
 
   class ConfigInstaller
-    def initialize(api_client)
-      @api_client = api_client
+    attr_reader :install_type
+
+    def initialize(api_client, options)
+      @api_client, @options = api_client, options
+      @install_dir, @install_type = nil, nil
+    end
+
+    def validate!
+      install_dir
     end
 
     def install_dir
+      unless @install_dir
+        @install_dir, @install_type = select_install_dir
+      end
+      @install_dir
+    end
+
+    def install_all
+      puts "\nInstalling..."
+
+      puts "> mkdir -p #{install_dir}"
+      FileUtils.mkdir_p(install_dir)
+
+      knife_rb = "#{@install_dir}/knife.rb"
+      puts "> create #{knife_rb} 0644"
+      File.open(knife_rb, File::RDWR|File::CREAT, 0644) {|f|
+        f << @api_client.knife_config
+      }
+
+      user_pem = "#{@install_dir}/#{@options.username}.pem"
+      puts "> create #{user_pem} 0600"
+      File.open(user_pem, File::RDWR|File::CREAT, 0600) {|f|
+        f << @api_client.user_key
+      }
+
+      validator_pem = "#{@install_dir}/#{@options.orgname}-validator.pem"
+      puts "> create #{validator_pem} 0600"
+      File.open(validator_pem, File::RDWR|File::CREAT, 0600) {|f|
+        f << @api_client.validator_key
+      }
+    end
+
+    private
+
+    def select_install_dir
       home_dot_chef = File.expand_path("~/.chef")
       cwd_dot_chef  = File.expand_path(".chef")
 
       if ENV['USER'] && !File.exist?(home_dot_chef)
-        home_dot_chef
-      elsif !File.exist(cwd_dot_chef)
+        [home_dot_chef, :default]
+      elsif !File.exist?(cwd_dot_chef)
+        [cwd_dot_chef, :non_default]
       else
+        install_locations = [home_dot_chef, cwd_dot_chef].uniq
+        if install_locations.size == 1
+          STDERR.puts(<<-NOWHERE_TO_GO)
+ERROR: You already have a Chef configuration in your home directory
+(#{home_dot_chef})
+
+If you really want to replace this configuration, you can remove it,
+otherwise cd to a different directory to generate a new config.
+NOWHERE_TO_GO
+        else
+          STDERR.puts(<<-NOWHERE_TO_GO)
+ERROR: You already have a Chef configuration in your home directory and
+your current working directory.
+(#{home_dot_chef} and #{cwd_dot_chef})
+
+If you wish to replace one of these you can remove it, otherwise you can
+cd to a different directory to create a new config.
+NOWHERE_TO_GO
+          exit 1
+        end
       end
     end
   end
@@ -222,17 +314,22 @@ module HostedChef
 
     def initialize(argv=ARGV)
       @argv = argv
+      @options = ArgvParser.new(argv).parse
+      @api_client = ApiClient.new(@options)
+      @validator = ConfigValidator.new(@api_client, @options)
+      @config_installer = ConfigInstaller.new(@api_client, @options)
     end
 
     def greeting
       puts(<<-WELCOME)
-## WELCOME! ##
-Welcome to Opscode Hosted Chef. We're going to download your API keys and knife
-config from Opscode and install them on your system.
+Welcome to Opscode Hosted Chef. We're going to download your API keys
+and knife config from Opscode and install them on your system.
 
 Before you continue, please be aware that this program will update your API key
 and your organization's validation key. The existing keys for these accounts
 will be expired and no longer valid.
+
+Your config will be created in #{@config_installer.install_dir}
 
 WELCOME
 
@@ -243,18 +340,11 @@ WELCOME
     end
 
     def setup_and_validate
-      @options = ArgvParser.new(argv).parse
-      @api_client = ApiClient.new(@options)
-      @validator = ConfigValidator.new(@api_client, @options)
       @validator.validate!
     end
 
     def fetch_and_install
-      puts "knife config: "
-      puts @api_client.knife_config
-      puts ""
-      puts "user key:"
-      puts @api_client.user_key
+      @config_installer.install_all
     end
 
     def goodbye
@@ -272,16 +362,20 @@ RESOURCES
 
 
     def run
+      @config_installer.validate!
       greeting
+      @options.ask_for_missing_opts
       setup_and_validate
       fetch_and_install
       goodbye
+    rescue Interrupt, EOFError
+      puts "\nexiting..."
+      exit! 1
     end
 
   end
 end
 
-HostedChef::Controller.new.run
 
 
 
